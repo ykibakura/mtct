ddd08e0073771ab4294db6ef79a1191281f609d0
ykibakura <ge78pak@mytum.de>
2024-07-02 14:19:15 +0200

    Status So 30.6

--- include/solver/a-star-based/AStarVSSPerformanceOptimizationSolver.hpp
+++ include/solver/a-star-based/AStarVSSPerformanceOptimizationSolver.hpp
@@ -22,7 +22,7 @@ namespace cda_rail::solver::astar_based {
     size_t num_vertices = 0;
 
     /////////////////////////
-    //train_pos, train_speed, train_routed defined in probleminstance Line 262
+    //train_pos, train_speed, train_routed defined in probleminstance Line 262?
     ////////////////////////
 
     struct train_state
@@ -49,6 +49,8 @@ namespace cda_rail::solver::astar_based {
       //initialises and return the initial state
       train_state initial_state;
       initial_state.train_pos = std::vector<int>(num_tr, start_pos);
+      initial_state.train_speed = std::vector<int>(num_tr, 0);
+      initial_state.train_routed = std::vector<int>(num_tr, false);
       //start_pos is the vector<num_tr, startpos_of_train>
       initial_state.n = 0;
       return initial_state;
@@ -56,12 +58,12 @@ namespace cda_rail::solver::astar_based {
 
     bool goal_state(train_state& other) {
       //checks if all train_pos match with the pre-defined goal_pos
-      bool operator == (train_state& other) const{
-        return goal_state,train_pos == std::vector<int>(num_tr, goal_pos);
+      bool operator == (train_state& state) const{
+        return state.train_pos == state.goal_pos
       }
+      //start_pos is the vector<num_tr, startpos_of_train>
       //goal_pos is the vector<num_tr, goalpos_of_train>
-      //write condition
-
+      //goal_state, when start_pos=goal_pos
     }
 
 
@@ -77,6 +79,48 @@ namespace cda_rail::solver::astar_based {
           num_tr = instance.get_num_tr();
           num_edges = instance.get_num_edges();
           num_vertices = instance.get_num_vertices();
+
+
+          const auto tr_list = instance.get_train_list();
+          const auto numtr = tr_list.size();
+          for (size_t i = 0; i < numtr; i++) {
+            const auto tr = tr_list.get_train(i);
+            const auto trl = tr.length;
+
+            const auto trs = instance.get_schedule(i);
+            const auto entry_vertex = trs.get_entry();
+
+            const auto network = instance.const_n();
+            const auto v0 = network.get_edge(0).source;
+            const auto v1 = network.get_edge(0).target;
+            // Edge 0 = v0 -> v1
+
+            const auto er = instance.const_n().get_reverse_edge_index(0);
+            if (er.has_value()) {
+              //er.value() ist index von v1 -> v0
+            } else {
+              // v1 -> v0 existiert nicht
+            }
+
+            // Zwei Importvarianten
+            // SimpleStation ist sehr überschaubar
+            // -------------------------------
+            //      \--------------/
+            const auto instance_imported = cda_rail::instances::GeneralPerformanceOptimizationInstance("path_to_instance_example_gen-po");
+            const auto instance_imported_2_vss = cda_rail::instances::VSSGenerationTimetable("path_to_example-network");
+            const auto instance_imported_2 = cda_rail::instances::GeneralPerformanceOptimizationInstance::cast_from_vss_generation(instance_imported_2_vss);
+
+
+            // Etwas ganz zum Schluss
+            bool tr_has_route = instance.has_route(tr.name); // true wenn die Kanten, die tr nutzt bereits festgelegt sind
+            // Aber, die sollten ignoriert werden
+            const auto tr_route = instance.get_route(tr.name);
+
+            // Wichtiger am Ende in der Lösung. Im Lösungsobjekt:
+            // sol.reset_routes();
+            // Für jeden Zug: .add_empty_route(...) und .push_back_edge_to_route(...)
+
+          }
         }
     explicit AStarVSSPerformanceOptimizationSolver(
         const std::filesystem::path& p)
@@ -84,9 +128,7 @@ namespace cda_rail::solver::astar_based {
           //
         }
     explicit AStarVSSPerformanceOptimizationSolver(const std::string& path);
-    explicit AStarVSSPer
-
-    formanceOptimizationSolver(const char* path);
+    explicit AStarVSSPerformanceOptimizationSolver(const char* path);
 
     // TODO: Implement missing functions
 
